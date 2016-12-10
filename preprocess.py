0f3ac3f2347aea6387457bfcd27e4b976de94657
HuChengkai <hucheng@umich.edu>
2016-12-09 22:22:48 -0500

    Preprocess the image

---
+++ preprocess.py
@@ -0,0 +1,25 @@
+'''
+Given an input image preprocess the image
+Apply grayscale, gaussian_filter, median filter
+'''
+import numpy as np
+from PIL import Image
+import scipy.misc
+import scipy.ndimage.filters
+import scipy.signal
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+def preprocess(infile):
+    img = np.asarray(Image.open(infile).convert('RGB'))     # read in image
+    img = rgb2gray(img)     # turn the image into grayscale
+    G_rst = scipy.ndimage.filters.gaussian_filter(img, 1,order=0, output=None,
+                                        mode='reflect', cval=0.0, truncate=4.0)
+    # apply median filter
+    M_rst = scipy.signal.medfilt(G_rst, kernel_size=3)
+    return M_rst
+
+if __name__ == '__main__':
+    out = preprocess('handwrite.png')
+    scipy.misc.imsave('preRest.png', out)
